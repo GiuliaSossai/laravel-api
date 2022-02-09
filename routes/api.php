4a2de70c5eecfd09a8252ee190899eefbb2f2330
GiuliaSossai <giulia.sossai@yahoo.it>
2022-02-09 16:35:04 +0100

    divisione cartelle scs e js tra front e back end

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('posts', 'Api\PostController@index');
+//per vederla vado nella pagina guest e nell'url aggiungno api/posts
+
+
